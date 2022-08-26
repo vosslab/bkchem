57612e07ec9f60d68236466a15d19cfd7c146154
Neil Voss <vossman77@yahoo.com>
2022-08-26 18:00:26 -0500

    collections.Callable -> collections.abc.Callable

--- bkchem/main.py
+++ bkchem/main.py
@@ -1369,7 +1369,9 @@ Enter InChI:""")
     for temp in self.menu_template:
       if temp[1] == "command" and temp[6] is not None:
         state = temp[6]
-        if isinstance(state, collections.Callable):
+        #print(temp)
+        #print(state)
+        if isinstance(state, collections.abc.Callable):
           state = state() and 'normal' or 'disabled'
         elif state not in  ('normal', 'disabled'):
           state = getattr( self.paper, temp[6]) and 'normal' or 'disabled'
